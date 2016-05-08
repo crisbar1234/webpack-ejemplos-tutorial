c7d7e95925122f574a41e6d9c55276e587eca5e9
juanda99 <juandacorreo@gmail.com>
2016-05-08 23:43:23 +0200

    añadido ejemplo con sass

---
+++ loader_con_sass/app/component.js
@@ -0,0 +1,7 @@
+'use strict';
+require("./component.scss");
+module.exports = function () {
+    var element = document.createElement('h1');
+    element.innerHTML = 'Hola Mundo';
+    return element;
+};
